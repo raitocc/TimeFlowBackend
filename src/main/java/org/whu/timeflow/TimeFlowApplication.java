f87ec0997d0935159bdd4a7ccc5abeb870d93095
raitocc <raitocc@qq.com>
2025-12-24 16:31:59 +0800

    feat(project): 初始化TimeFlow项目基础结构

---
+++ src/main/java/org/whu/timeflow/TimeFlowApplication.java
@@ -0,0 +1,13 @@
+package org.whu.timeflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimeFlowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/whu/timeflow/TimeFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package org.whu.timeflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
